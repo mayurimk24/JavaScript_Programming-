58b9b57ebacdd1bbd3527d816dd0e9b1cb1fdd83
Mayuri Mhaske <mayurimhaske24@gmail.com>
2026-04-03 20:03:39 +0530

    Create object

---
+++ Program40.js
@@ -0,0 +1,29 @@
+//random number representing a dice roll.
+
+let dice = Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+//obejct create a car
+
+const car = {
+      name: "Maruti Suzuki",
+      model: "Maruti Suzuki Dzire",
+      color: "White"
+};
+
+console.log(car);
+console.log(car.name);
+
+//create object person
+
+const person = {
+     name: "Ram",
+     age: 23,
+     city: "Pune"
+};
+
+console.log(person);
+person.city="New York";
+console.log(person.city);
+person.country = "United States";
+console.log(person.country);
