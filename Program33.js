857107c76cb399c0d1ebf3e9f52961cf65defd14
Mayuri Mhaske <mayurimhaske24@gmail.com>
2026-04-03 19:54:46 +0530

    Array with loop

---
+++ Program33.js
@@ -0,0 +1,17 @@
+let fruits = ["banana", "Apple", "Pineapple", "Mango"];
+
+for(let i = 0; i < fruits.length; i++)
+{
+    console.log(i, fruits[i]);
+}
+
+
+let arr = [['a','b','c','d'],['e','f','g','h']];
+
+for(let i = 0; i < arr.length; i++)
+{
+    for(let j = 0; j < arr[i].length; j++)
+    {
+        console.log(arr[i][j]);
+    }
+}
---
+++ Program34.js
@@ -0,0 +1,15 @@
+let fruits = ["banana", "Apple", "Pineapple", "Mango"];
+
+for(fruit of fruits)
+{
+    console.log(fruit);
+}
+
+let Characters = [['a', 'b','c','d'],['e','f','g','h']];
+
+for(char of Characters)
+{
+    for(charr of char){
+        console.log(charr);
+    }
+}
